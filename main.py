64c431796593c3ec6ce6864abdcfa65abb1074d4
aloeleaf <szabolcs.sandor@gmail.com>
2023-06-13 15:38:58 +0200

    Day 24: Exercise: Add lines

--- main.py
+++ main.py
@@ -13,20 +13,29 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
 
+    #s Set content of page
+    for i in range(20, 277, 10):
+        pdf.line(x1=10, y1=i, x2=200, y2=i)
+
     # Set footer
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
-    pdf.ln(265)
-    pdf.set_font(family="Times", style="I", size=24)
+    pdf.ln(269)
     pdf.set_text_color(180, 180, 180)
-    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    pdf.set_font(family="Times", style="I", size=24)
+    pdf.line(x1=10, y1=281, x2=200, y2=281)
+    pdf.cell(w=0, txt=row["Topic"], align="R")
   
     for page in range(row["Pages"] - 1):
         pdf.add_page()
+
+        #s Set content of page
+        for i in range(10, 277, 10):
+            pdf.line(x1=10, y1=i, x2=200, y2=i)
         # Set footer
-        pdf.line(x1=10, y1=21, x2=200, y2=21)
-        pdf.ln(277)
+        pdf.ln(281)
+        pdf.line(x1=10, y1=281, x2=200, y2=281)
         pdf.set_font(family="Times", style="I", size=24)
         pdf.set_text_color(180, 180, 180)
-        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        pdf.cell(w=0, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
+
